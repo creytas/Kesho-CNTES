b9a70fda0bd5d04f747903d3473515f9530d0afc
CR3Y745 <creytas@gmail.com>
2022-01-24 10:59:31 +0100

    fix: reframe charts data source

--- src/components/_dashboard/app/AppCurrentVisits.js
+++ src/components/_dashboard/app/AppCurrentVisits.js
@@ -1,49 +1,49 @@
-import { useState, useEffect } from 'react';
-import { merge } from 'lodash';
-import ReactApexChart from 'react-apexcharts';
-import Axios from 'axios';
-import moment from 'moment';
+import { useState, useEffect } from "react";
+import { merge } from "lodash";
+import ReactApexChart from "react-apexcharts";
+import Axios from "axios";
+import moment from "moment";
 // material
-import { useTheme, styled } from '@material-ui/core/styles';
-import { Card, CardHeader } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import { useTheme, styled } from "@material-ui/core/styles";
+import { Card, CardHeader } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
 // utils
-import { fNumber } from '../../../utils/formatNumber';
+import { fNumber } from "../../../utils/formatNumber";
 //
-import { BaseOptionChart } from '../../charts';
+import { BaseOptionChart } from "../../charts";
 
 // ----------------------------------------------------------------------
 
 const CHART_HEIGHT = 372;
 const LEGEND_HEIGHT = 72;
 
-const ChartWrapperStyle = styled('div')(({ theme }) => ({
+const ChartWrapperStyle = styled("div")(({ theme }) => ({
   height: CHART_HEIGHT,
   marginTop: theme.spacing(5),
-  '& .apexcharts-canvas svg': { height: CHART_HEIGHT },
-  '& .apexcharts-canvas svg,.apexcharts-canvas foreignObject': {
-    overflow: 'visible'
+  "& .apexcharts-canvas svg": { height: CHART_HEIGHT },
+  "& .apexcharts-canvas svg,.apexcharts-canvas foreignObject": {
+    overflow: "visible",
   },
-  '& .apexcharts-legend': {
+  "& .apexcharts-legend": {
     height: LEGEND_HEIGHT,
-    alignContent: 'center',
-    position: 'relative !important',
+    alignContent: "center",
+    position: "relative !important",
     borderTop: `solid 1px ${theme.palette.divider}`,
-    top: `calc(${CHART_HEIGHT - LEGEND_HEIGHT}px) !important`
-  }
+    top: `calc(${CHART_HEIGHT - LEGEND_HEIGHT}px) !important`,
+  },
 }));
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2)
-    }
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
   },
   labelRoot: {
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
 }));
 
 export default function AppCurrentVisits() {
@@ -53,27 +53,41 @@ export default function AppCurrentVisits() {
   const [masData, setMasData] = useState(0);
   const [gueris, setGueris] = useState(0);
   const [loader, setLoader] = useState(true);
-  const currentYear = moment().format('YYYY');
+  const currentYear = moment().format("YYYY");
   useEffect(async () => {
     try {
-      const response = await Axios.get(`https://kesho-congo-api.herokuapp.com/reporting/annuel`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `bearer ${localStorage.getItem('token')}`
+      const response = await Axios.get(
+        `https://kesho-api.herokuapp.com/reporting/annuel`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${localStorage.getItem("token")}`,
+          },
         }
-      });
+      );
       const output = await response.data;
       setMasData(
-        await output.rapport_mas_year.map((i) => i[0].sereve_nombre).reduce((a, b) => a + b)
+        await output.rapport_mas_year
+          .map((i) => i[0].sereve_nombre)
+          .reduce((a, b) => a + b)
       );
       setMamData(
-        await output.rapport_mam_year.map((i) => i[0].moderee_nombre).reduce((a, b) => a + b)
+        await output.rapport_mam_year
+          .map((i) => i[0].moderee_nombre)
+          .reduce((a, b) => a + b)
       );
+      console.log(mamData);
+      console.log(masData);
+      console.log(macData);
       setMacData(
-        await output.rapport_mac_year.map((i) => i[0].chronique_nombre).reduce((a, b) => a + b)
+        await output.rapport_mac_year
+          .map((i) => i[0].chronique_nombre)
+          .reduce((a, b) => a + b)
       );
       setGueris(
-        await output.rapport_gueri_year.map((i) => i[0].nombre_patient).reduce((a, b) => a + b)
+        await output.rapport_gueri_year
+          .map((i) => i[0].gueri_nombre)
+          .reduce((a, b) => a + b)
       );
       setLoader(false);
     } catch (err) {
@@ -82,7 +96,7 @@ export default function AppCurrentVisits() {
     }
   }, []);
 
-  const CHART_DATA = [macData, mamData, masData, gueris];
+  const CHART_DATA = [macData, masData, mamData, gueris];
   const theme = useTheme();
   // const CHART_DATA = [
   //   macData.map((i) => i[0].chronique_nombre).reduce((a, b) => a + b),
@@ -95,36 +109,44 @@ export default function AppCurrentVisits() {
       theme.palette.error.main,
       theme.palette.error.light,
       theme.palette.warning.main,
-      theme.palette.primary.main
+      theme.palette.primary.main,
     ],
-    labels: ['MAC', 'MAS', 'MAM', 'Guéris'],
+    labels: ["MAC", "MAS", "MAM", "Guéris"],
     stroke: { colors: [theme.palette.background.paper] },
-    legend: { floating: true, horizontalAlign: 'center' },
+    legend: { floating: true, horizontalAlign: "center" },
     dataLabels: { enabled: true, dropShadow: { enabled: false } },
     tooltip: {
       fillSeriesColor: false,
       y: {
         formatter: (seriesName) => fNumber(seriesName),
         title: {
-          formatter: (seriesName) => `#${seriesName}`
-        }
-      }
+          formatter: (seriesName) => `#${seriesName}`,
+        },
+      },
     },
     plotOptions: {
-      pie: { donut: { labels: { show: false } } }
-    }
+      pie: { donut: { labels: { show: false } } },
+    },
   });
 
   return (
     <Card>
-      <CardHeader className={classes.labelRoot} title={`Ratio Annuel ${currentYear}`} />
+      <CardHeader
+        className={classes.labelRoot}
+        title={`Ratio Annuel ${currentYear}`}
+      />
       {loader ? (
         <div className={classes.root}>
           <LinearProgress />
         </div>
       ) : (
         <ChartWrapperStyle dir="ltr">
-          <ReactApexChart type="pie" series={CHART_DATA} options={chartOptions} height={280} />
+          <ReactApexChart
+            type="pie"
+            series={CHART_DATA}
+            options={chartOptions}
+            height={280}
+          />
         </ChartWrapperStyle>
       )}
     </Card>
--- src/components/_dashboard/app/AppWebsiteVisits.js
+++ src/components/_dashboard/app/AppWebsiteVisits.js
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react';
-import { merge } from 'lodash';
-import ReactApexChart from 'react-apexcharts';
-import Axios from 'axios';
-import moment from 'moment';
-import { useTheme } from '@material-ui/core/styles';
+import { useEffect, useState } from "react";
+import { merge } from "lodash";
+import ReactApexChart from "react-apexcharts";
+import Axios from "axios";
+import moment from "moment";
+import { useTheme } from "@material-ui/core/styles";
 // material
-import { Card, CardHeader, Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import { Card, CardHeader, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
 //
-import { BaseOptionChart } from '../../charts';
+import { BaseOptionChart } from "../../charts";
 
 // ----------------------------------------------------------------------
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2)
-    }
-  }
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
 }));
 
 export default function AppWebsiteVisits() {
@@ -29,16 +29,19 @@ export default function AppWebsiteVisits() {
   const [gueris, setGueris] = useState([]);
   const [repData, setRepData] = useState([]);
   const [loader, setLoader] = useState(true);
-  const currentYear = moment().format('YYYY');
+  const currentYear = moment().format("YYYY");
 
   useEffect(async () => {
     try {
-      const response = await Axios.get(`https://kesho-congo-api.herokuapp.com/reporting/annuel`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `bearer ${localStorage.getItem('token')}`
+      const response = await Axios.get(
+        `https://kesho-api.herokuapp.com/reporting/annuel`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${localStorage.getItem("token")}`,
+          },
         }
-      });
+      );
       const data = await response.data;
       setRepData(await data.rapport_patient_year);
       setMasData(await data.rapport_mas_year);
@@ -55,30 +58,30 @@ export default function AppWebsiteVisits() {
   }, []);
   const CHART_DATA = [
     {
-      name: 'Nombre de patients',
-      type: 'column',
-      data: repData.map((i) => i[0].nombre_patient)
+      name: "Nombre de patients",
+      type: "column",
+      data: repData.map((i) => i[0].nombre_patient),
     },
     {
-      name: 'MAC',
-      type: 'line',
-      data: macData.map((i) => i[0].chronique_nombre)
+      name: "MAC",
+      type: "line",
+      data: macData.map((i) => i[0].chronique_nombre),
     },
     {
-      name: 'MAS',
-      type: 'line',
-      data: masData.map((i) => i[0].sereve_nombre)
+      name: "MAS",
+      type: "line",
+      data: masData.map((i) => i[0].sereve_nombre),
     },
     {
-      name: 'MAM',
-      type: 'line',
-      data: mamData.map((i) => i[0].moderee_nombre)
+      name: "MAM",
+      type: "line",
+      data: mamData.map((i) => i[0].moderee_nombre),
     },
     {
-      name: 'Guéris',
-      type: 'line',
-      data: gueris.map((i) => i[0].nombre_patient)
-    }
+      name: "Guéris",
+      type: "line",
+      data: gueris.map((i) => i[0].gueri_nombre),
+    },
   ];
   const theme = useTheme();
   const chartOptions = merge(BaseOptionChart(), {
@@ -87,49 +90,57 @@ export default function AppWebsiteVisits() {
       theme.palette.error.main,
       theme.palette.error.light,
       theme.palette.warning.main,
-      theme.palette.primary.main
+      theme.palette.primary.main,
     ],
     stroke: { width: [0, 3, 3, 3, 3] },
-    plotOptions: { bar: { columnWidth: '10%', borderRadius: 4 } },
-    fill: { type: ['solid', 'gradient', 'solid', 'solid', 'solid'] },
+    plotOptions: { bar: { columnWidth: "10%", borderRadius: 4 } },
+    fill: { type: ["solid", "gradient", "solid", "solid", "solid"] },
     labels: [
-      'Janvier',
-      'Février',
-      'Mars',
-      'Avril',
-      'Mai',
-      'Juin',
-      'Juillet',
-      'Août',
-      'Septembre',
-      'Octobre',
-      'Novembre',
-      'Decembre'
+      "Janvier",
+      "Février",
+      "Mars",
+      "Avril",
+      "Mai",
+      "Juin",
+      "Juillet",
+      "Août",
+      "Septembre",
+      "Octobre",
+      "Novembre",
+      "Decembre",
     ],
     tooltip: {
       shared: true,
       intersect: false,
       y: {
         formatter: (y) => {
-          if (typeof y !== 'undefined') {
+          if (typeof y !== "undefined") {
             return `${y.toFixed(0)} patients`;
           }
           return y;
-        }
-      }
-    }
+        },
+      },
+    },
   });
 
   return (
     <Card>
-      <CardHeader title="Visualisation annuelle" subheader={`Kesho Congo ${currentYear}`} />
+      <CardHeader
+        title="Visualisation annuelle"
+        subheader={`Kesho Congo ${currentYear}`}
+      />
       {loader ? (
         <div className={classes.root}>
           <LinearProgress />
         </div>
       ) : (
         <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-          <ReactApexChart type="line" series={CHART_DATA} options={chartOptions} height={364} />
+          <ReactApexChart
+            type="line"
+            series={CHART_DATA}
+            options={chartOptions}
+            height={364}
+          />
         </Box>
       )}
     </Card>
